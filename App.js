e57be108081d6587c21fa7b10466cca216d64b88
KIMSEOLHEE <ksh7621@naver.com>
2021-01-29 02:02:15 +0900

    new!

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-// import * as React from 'react';
-// import { NavigationContainer, } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import Home from './components/Home';
-// import Calendar from './components/Calendar';
-// import Agenda from './components/Agenda';
-
-
-
-// const Stack = createStackNavigator();
-
-// function App() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen name="Home" component={Home} />
-//         <Stack.Screen name="Calendar" component={Calendar} />
-//         <Stack.Screen name="Agenda" component={Agenda} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-// export default App;
-
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
